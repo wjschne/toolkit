69116d259bc1b84569d816ef6396d75cdba1f1f8
W. Joel Schneider <w.joel.schneider@gmail.com>
2021-04-04 15:56:19 -0400

    composites

--- Useful stuff.R
+++ Useful stuff.R
@@ -1,5 +1,18 @@
-<span class = 'defword'>*\1*</span>
-
 \\defword{(.*?)}
 
+
+[\1]{class=defword}
+
+\\marginnote{(.*?)}
+
+`r tufte::margin_note(
+
+{<<.*?>>}
+
+
+\\marginnote{(.*?)}
+
+\[(.*?)\]\{class=defword\}`r defword\(
+
+  ,`r defword("\1",
 
